439931c8acad3cbcacdaa3ce0c82e6a3d01572c8
Peter Edwards <peadar@arista.com>
2022-12-09 14:35:36 +0000

    clean up

--- 9/solve.cc
+++ 9/solve.cc
@@ -3,19 +3,18 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <map>
 #include <set>
+#include <cassert>
 
 class Coord {
     int x, y;
 public:
+    constexpr bool operator < (const Coord &rhs) const { return x < rhs.x || (x == rhs.x && y < rhs.y); }
     constexpr Coord &operator += (const Coord &rhs) {
         x += rhs.x;
         y += rhs.y;
         return *this;
     }
-    constexpr bool operator == (const Coord &rhs) const { return x == rhs.x && y == rhs.y; }
-    constexpr bool operator < (const Coord &rhs) const { return x < rhs.x || (x == rhs.x && y < rhs.y); }
     constexpr bool adjacent(const Coord &rhs) const { return std::abs(x - rhs.x) <= 1 && std::abs(y - rhs.y) <= 1; }
     constexpr void movetoward(const Coord &target) {
         x += target.x == x ? 0 : target.x > x ? 1 : -1;
@@ -25,26 +24,34 @@ public:
     Coord() = default;
 };
 
-int
-main(int, char *argv[])
-{
-    static std::map<char, Coord > directions = {
-        { 'U', {0, -1} }, { 'D', {0, 1} }, { 'L', {-1, 0} }, { 'R', {1, 0} }
-    };
-    static constexpr int partSizes[] { 2, 10 };
-    for (int part = 0; part < 2; ++part) {
-        std::vector<Coord> snake(partSizes[part]);
-        std::set<Coord> visited { snake.back() };
-        std::string s;
-        for (std::ifstream f(argv[1]); std::getline(f, s); ) {
-            auto &dir = directions[s[0]];
-            for (auto i = stoi(s.substr(2)); i; --i) {
-                snake[0] += dir;
-                for (size_t pos = 1; pos < snake.size() && !snake[pos].adjacent(snake[pos-1]); ++pos)
-                    snake[pos].movetoward(snake[pos-1]);
-                visited.insert(snake.back());
-            }
-        }
-        std::cout << "part " << part + 1 << " " << visited.size() << "\n";
+static Coord direction(char d) {
+    switch (d) {
+        case 'U': return {  0, -1 };
+        case 'D': return {  0,  1 };
+        case 'L': return { -1,  0 };
+        case 'R': return {  1,  0 };
+        default: throw std::runtime_error("invalid direction");
     }
+}
+
+int solve(std::fstream input, int length) {
+    assert(length > 0);
+    std::vector<Coord> snake(length);
+    std::set<Coord> visited { snake.back() };
+    for (std::string s; std::getline(input, s); ) {
+        Coord dir = direction(s[0]);
+        for (auto i = stoi(s.substr(2)); i; --i) {
+            snake[0] += dir;
+            for (size_t pos = 1; pos < snake.size() && !snake[pos].adjacent(snake[pos-1]); ++pos)
+                snake[pos].movetoward(snake[pos-1]);
+            visited.insert(snake.back());
+        }
+    }
+    return visited.size();
+}
+
+int main(int, char *argv[]) {
+    int sizes[] { 2, 10 };
+    for (int i = 0; i < 2; ++i)
+        std::cout << "part " << i + 1 << " " << solve(std::fstream(argv[1]), sizes[i]) << "\n";
 }
